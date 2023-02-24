eb982467251a91a3138e9789f578feb422baa508
anastasia <moiri@mail.ru>
2023-02-24 20:23:20 +0100

    Lesson 4 done

--- todo_notes/todo_app/views.py
+++ todo_notes/todo_app/views.py
@@ -2,14 +2,42 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, TODO
 from .serializers import ProjectModelSerializer, TODOModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from .filters import ProjectFilter, TODOFilter
+from rest_framework.response import Response
+from rest_framework import status
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
 
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TODOModelViewSet(ModelViewSet):
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+    pagination_class = TODOLimitOffsetPagination
+    filterset_fields = ['project']
+
+    def destroy(self, request, *args, **kwargs):
+        partial = True
+        instance = self.get_object()
+        instance.is_active = 'False'
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
 
