f5262e046d4e515653cf60557f6be10f7190b636
anastasia <moiri@mail.ru>
2023-03-17 19:20:39 +0100

    Lesson_8 done

--- todo_notes/notes_users/views.py
+++ todo_notes/notes_users/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render
 from rest_framework.renderers import JSONRenderer
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework import permissions
 
 
 from .models import NotesUser
 from .serializers import NotesUserModelSerializer
 
-class NotesUserModelViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin,
-    mixins.RetrieveModelMixin, viewsets.GenericViewSet):
-    queryset = NotesUser.objects.all()
-    serializer_class = NotesUserModelSerializer
-
-# class NotesUserModelViewSet(viewsets.ModelViewSet):
+# class NotesUserModelViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin,
+#     mixins.RetrieveModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
 #     queryset = NotesUser.objects.all()
 #     serializer_class = NotesUserModelSerializer
+
+class NotesUserModelViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+    queryset = NotesUser.objects.all()
+    serializer_class = NotesUserModelSerializer
 
